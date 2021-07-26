c8e26fb276482d2dfddf2631b05aa5770dd2fa90
Anju <anjumariya7@gmail.com>
2021-07-26 21:39:12 +0530

    test

--- File/fileprogram1.py
+++ File/fileprogram1.py
@@ -7,10 +7,11 @@ for i in f1:    # code to get the data from file
     print(i)
 
 
-f2=open("file2","w")    # open file file2 in write mode. in this mode existing content will be replaced by bew content.
-                        # if the file is not present it will create a new file in ythe same name and write the content to it
-f2.write("hello")
+# f2=open("file2","w")    # open file file2 in write mode. in this mode existing content will be replaced by bew content.
+#                         # if the file is not present it will create a new file in ythe same name and write the content to it
+# f2.write("hello")
+#
+#
+# f3=open("file2","a")    # open file file2 in append mode. in append mode add the data to existind data
+# f2.write("hello")
 
-
-f3=open("file2","a")    # open file file2 in append mode. in append mode add the data to existind data
-f2.write("hello")
